13c52908180ee0bbc354e20c2321121e757af38e
Tianyong Tang <tang3w@gmail.com>
2015-04-23 08:03:17 +0800

    Make symbol uploading more robust

--- lib/leancloud/http/symbol_uploader.rb
+++ lib/leancloud/http/symbol_uploader.rb
@@ -14,17 +14,38 @@ module LeanCloud
 
     private
 
-    def verbose
-      @options[:verbose]
+    attr_reader :dsym_path
+    attr_reader :dsym_macho
+    attr_reader :dsym_archs
+    attr_reader :tmp_dir
+
+    def verbose?
+      @verbose ||= !@options[:verbose].nil?
     end
 
     def dsym_path
-      path = @options[:file]
-      @dsym_path ||= path if !path.nil? and File.readable?(path)
+      @dsym_path ||= (
+        path = @options[:file]
+        path if !path.nil? and File.readable?(path)
+      )
     end
 
     def dsym_macho
       @dsym_macho ||= Dir.glob(File.join(dsym_path, 'Contents/Resources/DWARF/*')).first
+    end
+
+    def lipo_dsym_archs
+      info = %x(lipo -info #{dsym_macho} 2>/dev/null)
+      arch_list = info[/[^:]+$/].strip
+      arch_list.split(' ')
+    end
+
+    def dsym_archs
+      @dsym_archs ||= lipo_dsym_archs
+    end
+
+    def tmp_dir
+      @tmp_dir ||= File.join(Dir.tmpdir(), 'cn.leancloud/symbols')
     end
 
     def validate_options
@@ -33,20 +54,14 @@ module LeanCloud
       report_error('DSYM file not found') unless dsym_path
     end
 
-    def tmp_dir
-      @tmp_dir ||= File.join(Dir.tmpdir(), 'cn.leancloud/symbols')
-    end
-
-    def dsym_archs
-      info = `lipo -info #{dsym_macho}`
-      arch_list = info[/(?<=:)([^:]*)$/].strip
-      arch_list.split(' ')
+    def symbol_path(arch)
+      File.join(tmp_dir, "#{arch}.sym")
     end
 
     def dump_cmd_template
       <<-EOT.gsub(/^[ \t]+/, '')
       {{#archs}}
-      leancloud_dump_syms -a {{name}} #{dsym_path} > #{tmp_dir}/{{name}}.sym
+      leancloud_dump_syms -a {{name}} #{dsym_path} > {{path}} 2>/dev/null
       {{/archs}}
       EOT
     end
@@ -54,11 +69,11 @@ module LeanCloud
     def dump_symbol
       FileUtils.mkdir_p(tmp_dir)
 
-      cmd = Mustache.render(dump_cmd_template, {
-        archs: dsym_archs.map { |arch| { 'name' => arch } }
-      })
+      archs = dsym_archs.map { |arch| { name: arch, path: symbol_path(arch) } }
 
-      puts "Command for dump symbol file:\n#{cmd}" if verbose
+      cmd = Mustache.render(dump_cmd_template, archs: archs)
+
+      puts "Command for dump symbol files:\n#{cmd}" if verbose?
 
       system(cmd)
     end
@@ -67,7 +82,7 @@ module LeanCloud
       fields = []
 
       dsym_archs.each do |arch|
-        path = "#{tmp_dir}/#{arch}.sym"
+        path = symbol_path(arch)
         next if !File.readable?(path) or File.zero?(path)
         fields << "-F \"symbol_file_#{arch}=@#{path}\""
       end
@@ -91,8 +106,8 @@ module LeanCloud
       #{url} >/dev/null 2>&1
       EOC
 
+      puts "Command for uploading symbol files:\n#{cmd}" if verbose?
       puts 'Uploading symbol files...'
-      puts "Command for uploading:\n#{cmd}" if verbose
 
       unless system(cmd)
         report_error('Failed to upload symbol files.')
